74eabe49e2fc1b5d3addd4b33e9aa82ea3f1129d
Dongwoo Kim <dongwoo.kim@anu.edu.au>
2019-05-01 15:46:23 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='travistest',
-    packages=['travistest'],
+    packages=find_packages(),
     version='0.0.7',
     description='Test project to get acquainted with TravisCI',
     url='https://github.com/',    
